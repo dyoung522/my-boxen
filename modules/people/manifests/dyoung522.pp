d2ef479dedc3503ea7adf3e2cf3d0690c158dd9f
Donovan C. Young <dyoung522@gmail.com>
2013-09-18 17:15:17 -0400

    tweaks

--- modules/people/manifests/dyoung522.pp
+++ modules/people/manifests/dyoung522.pp
@@ -1,5 +1,5 @@
 class people::dyoung522 {
-  include people::dyoung522::applications
+#  include people::dyoung522::applications
 #  include people::dyoung522::properties
 #  include people::dyoung522::dotfiles
 
