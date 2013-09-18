02e8df5445fc1519a48d7c8083bfbc9184cd4f24
Donovan C. Young <dyoung522@gmail.com>
2013-09-18 16:57:40 -0400

    tweaks

--- modules/people/manifests/dyoung522.pp
+++ modules/people/manifests/dyoung522.pp
@@ -1,5 +1,4 @@
 class people::dyoung522 {
-  include people::dyoung522::packages
   include people::dyoung522::applications
   include people::dyoung522::properties
   include people::dyoung522::dotfiles
--- modules/people/manifests/dyoung522/applications.pp
+++ modules/people/manifests/dyoung522/applications.pp
@@ -1,4 +1,12 @@
 class people::dyoung522::applications {
+  # iTerm2 http://www.iterm2.com
+  package { 'iterm2':
+    ensure   => installed,
+    source   => 'http://www.iterm2.com/downloads/stable/iTerm2_v1_0_0.zip',
+    provider => compressed_app,
+  }
+
+  include zsh
   include alfred
   include postgresql
   include rubymine
