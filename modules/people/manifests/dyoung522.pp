a2ebc42cbdbedf001d2bb9fcd385284584327b68
Donovan C. Young <dyoung522@gmail.com>
2013-09-18 16:49:17 -0400

    update dyoung522.pp to include additional modules (whoops)

--- modules/people/manifests/dyoung522.pp
+++ modules/people/manifests/dyoung522.pp
@@ -1,9 +1,9 @@
-include people::dyoung522::packages
-include people::dyoung522::applications
-include people::dyoung522::properties
-include people::dyoung522::dotfiles
-
 class people::dyoung522 {
+  include people::dyoung522::packages
+  include people::dyoung522::applications
+  include people::dyoung522::properties
+  include people::dyoung522::dotfiles
+
   notify { 'class people::dyoung522 declared': }
 }
 
