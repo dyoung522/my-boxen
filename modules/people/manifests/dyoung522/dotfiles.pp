54a18b92ac2f845af66a27dfea2ec024feb72ba7
Donovan C. Young <dyoung522@gmail.com>
2013-09-18 17:37:24 -0400

    add dotfiles

--- modules/people/manifests/dyoung522/dotfiles.pp
+++ modules/people/manifests/dyoung522/dotfiles.pp
@@ -7,60 +7,9 @@ class people::dyoung522::dotfiles {
     source => 'dyoung522/dotfiles',
   }
 
-  file { "${my_homedir}/.gitconfig":
-    ensure  => link,
-    mode    => '0644',
-    target  => "${my_dotfiles}/gitconfig",
+  exec { "bootstrap dotfiles":
+    command => "bootstrap",
+    path    => "${my_dotfiles}/scripts/",
     require => Repository["${my_dotfiles}"],
   }
-
-  file { "${my_homedir}/.gitignore":
-    ensure  => link,
-    mode    => '0644',
-    target  => "${my_dotfiles}/gitignore",
-    require => Repository["${my_dotfiles}"],
-  }
-
-  file { "${my_homedir}/.tmux.conf":
-    ensure  => link,
-    mode    => '0644',
-    target  => "${my_dotfiles}/tmux.conf",
-    require => Repository["${my_dotfiles}"],
-  }
-
-  file { "${my_homedir}/.zsh":
-    ensure  => link,
-    mode    => '0755',
-    target  => "${my_dotfiles}/zsh",
-    require => Repository["${my_dotfiles}"],
-  }
-
-  file { "${my_homedir}/.zshrc":
-    ensure  => link,
-    mode    => '0644',
-    target  => "${my_dotfiles}/zshrc",
-    require => Repository["${my_dotfiles}"],
-  }
-
-  file { "${my_homedir}/.vim":
-    ensure => link,
-    mode   => '0755',
-    target => "${my_dotfiles}/vim",
-    require => Repository["${my_dotfiles}"],
-  }
-
-  file { "${my_homedir}/.vimrc":
-    ensure => link,
-    mode   => '0644',
-    target => "${my_dotfiles}/vimrc",
-    require => Repository["${my_dotfiles}"],
-  }
-
-  file { "${my_homedir}/bin":
-    ensure => link,
-    mode   => '0755',
-    target => "${my_dotfiles}/bin",
-    require => Repository["${my_dotfiles}"],
-  }
-
 }
