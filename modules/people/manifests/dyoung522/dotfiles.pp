ea7c89a7b7c7f678ac8fecbc3d83a4bb91029089
Donovan C. Young <dyoung522@gmail.com>
2013-09-19 10:52:25 -0400

    dotfile fixes

--- modules/people/manifests/dyoung522/dotfiles.pp
+++ modules/people/manifests/dyoung522/dotfiles.pp
@@ -3,12 +3,30 @@ class people::dyoung522::dotfiles {
   $my_homedir  = "/Users/${::boxen_user}"
   $my_dotfiles = "${my_homedir}/.dotfiles"
 
+  notify { "After the run, don't forget to run script/install from the .dotfiles directory": }
+
   repository { "${my_dotfiles}":
     source => 'dyoung522/dotfiles',
   }
 
-  exec { "install dotfiles":
-    command => "cd ${my_dotfiles}; script/install",
+#  exec { "install dotfiles":
+#    command => "cd ${my_dotfiles}; script/install",
+#    require => Repository["${my_dotfiles}"],
+#  }
+
+  # We're just going to symlink the important files for now, after that we can
+  # run our installer from the command line
+  file { "${my_homedir}/.zshrc":
+    ensure  => link,
+    mode    => '0644',
+    target  => "${my_dotfiles}/zsh/zshrc.symlink",
+    require => Repository["${my_dotfiles}"],
+  }
+
+  file { "${my_homedir}/bin":
+    ensure  => link,
+    mode    => '0644',
+    target  => "${my_dotfiles}/bin",
     require => Repository["${my_dotfiles}"],
   }
 }
